8db534d908d8e33c45d5def1e1db11acfa224fff
Mohmed-Ayoub-Js <clanning852@gmail.com>
2025-02-01 20:48:17 +0100

    update-github-button

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,17 +2,22 @@ import React from 'react'
 import { Button } from '../ui/button'
 import { Code, FileText, Github } from 'lucide-react'
 import { ModeToggle } from '../theme/mode-toggle'
+import Link from 'next/link'
 
 const Header = () => {
+  const githubLink = "https://github.com/mohammed-ayoub-javascript/poetry-next"
   return (
     <div className=' w-full flex justify-between items-center flex-row border p-3 mb-5'>
         <div className=' text-2xl font-bold ar'>
             الشاعر الحديث
         </div>
         <div className=' flex justify-center items-center flex-row gap-3'>
+            <Link target="_blank" href={githubLink}>
             <Button variant={"outline"}>
                 <Github />
-            </Button>
+            </Button>            
+            </Link>
+
             <ModeToggle />
             <Button variant={"outline"}>
                 <FileText />
